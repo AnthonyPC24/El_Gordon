f13fd63e1b78118861f0ecc8f3cb04390c9107e6
SrHect0r <hmudarral2425@politecnics.barcelona>
2025-11-07 15:35:11 +0100

    Commit 7/11/2025

--- app/src/main/java/com/example/el_gordon/MainActivity.kt
+++ app/src/main/java/com/example/el_gordon/MainActivity.kt
@@ -1,20 +1,19 @@
 package com.example.el_gordon
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val txtJugar = findViewById<TextView>(R.id.txtJugar)
+        txtJugar.setOnClickListener {
+            val intent = Intent(this, JuegoActivity::class.java)
+            startActivity(intent)
         }
     }
 }
